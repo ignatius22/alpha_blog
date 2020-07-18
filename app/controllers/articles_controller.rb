320a8ed7f26fcced9bbcfa54e6f5ab6160ab8abe
Ignatius22 <igee54077@gmail.com>
2020-07-18 06:58:41 +0100

    refactor code, using partials and other ruby method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def index
@@ -11,11 +11,11 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	def edit
-		@article = Article.find(params[:id])
+	
 	end
 
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(article_params)
 		flash[:notice] = "Article was created successfully"
 		if @article.save
 		redirect_to @article
@@ -23,9 +23,8 @@ class ArticlesController < ApplicationController
 		render 'new'
 	end
 	end
-	def update
-	 @article = Article.find(params[:id])
-	 if @article.update(params.require(:article).permit(:title, :description))
+	def update	 
+	 if @article.update(article_params)
 	 	flash[:notice] = "Article was edited successfully."
 	 	redirect_to @article
 	 else
@@ -33,8 +32,15 @@ class ArticlesController < ApplicationController
 	end
 end
 def destroy
-	@article = Article.find(params[:id])
+	
 	@article.destroy
 	redirect_to articles_path
+end
+private 
+def set_article
+	@article = Article.find(params[:id])
+end
+def article_params
+	params.require(:article).permit(:title, :description)
 end
 end
