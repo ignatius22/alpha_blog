4347fa412549713b3ea858e0c51a7a86f07e6065
Ignatius22 <igee54077@gmail.com>
2020-07-20 03:03:24 +0100

    add user sign up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,8 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		@article.user = User.first
-		flash[:notice] = "Article was created successfully"
 		if @article.save
+			flash[:notice] = "Article was created successfully"
 		redirect_to @article
 	else
 		render 'new'
