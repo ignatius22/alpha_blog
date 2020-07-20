016590d28d537477de3263261996f8db4631a8b6
Ignatius22 <igee54077@gmail.com>
2020-07-20 18:47:44 +0100

    add user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
+
+	def show
+	   @user = User.find(params[:id])
+	   @articles = @user.articles
+	end
+
 	def new
 		@user = User.new
 	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
