01b04aa10b316a9136a5ea4cfeb1ab5e09292be3
Ignatius22 <igee54077@gmail.com>
2020-07-21 18:21:13 +0100

    add admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:show, :edit, :update]
-	before_action :require_user, except: [:edit, :update]
-	before_action :require_same_user, only: [:edit, :update]
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+	before_action :require_user, only: [:edit, :update]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
 		@users = User.paginate(page: params[:page], per_page: 3)
@@ -37,6 +37,12 @@ class UsersController < ApplicationController
 	  end
 	end
 
+	def destroy
+		@user.destroy
+		session[:user_id] = nil if @user == current_user
+		flash[:notice] = "Account and all associated articles deleted successfully"
+		redirect_to articles_path
+	end
 
 	private
 	def user_params
@@ -47,8 +53,8 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 	def require_same_user
-		if current_user != @user
-			flash[:alert] = "you can only edit your own profile"
+		if current_user != @user && !current_user.admin?
+			flash[:alert] = "you can only edit or delete your own profile"
 			redirect_to @user
 		end
 	end
